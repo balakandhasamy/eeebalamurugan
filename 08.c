6e84a3c2acf35973131d3bcb07d5eca3224e4ad6
balakandhasamy <30549603+balakandhasamy@users.noreply.github.com>
2017-08-11 17:21:54 +0530

    Create 08.c

---
+++ 08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int z, i, sum = 0;
+    printf("Enter a  integer");
+    scanf("%d",&z);
+    for(i=1;i<= z;i++)
+    {
+        sum += i; 
+    }
+ printf("Sum = %d",sum);
+return 0;
+}
