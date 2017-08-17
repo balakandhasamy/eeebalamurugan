dc6111aee9227aade85c96c60379f424cbb437b3
balakandhasamy <30549603+balakandhasamy@users.noreply.github.com>
2017-08-17 15:26:06 +0530

    Update and rename 15a.c to 15a.cpp

---
+++ 15a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int y;
+ int z;
+cout<<"please enter num"<<endl;
+cin>>y;
+cout<<"enter num pleaseeeeeeeeeee program by programology site abdullah moeen"<<endl;
+cin>>z;
+while(y<z){
+ 
+ if(y%2==0)
+ {
+ cout<<"even no. ="<<y<<endl;
+ 
+ }
+ y=y+1;
+}
+}
